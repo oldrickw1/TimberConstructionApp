660b77211b3da0345cade2d85e78aa79f3cb9311
oldrickw1 <o.weenink@hotmail.com>
2024-11-06 16:01:04 +0100

    Add post endpoint (controller) that accepts a message, add Dockerfile

--- backend/RabbitMQMB/src/main/java/com/oldrick/RabbitMQMB/controller/MessageController.java
+++ backend/RabbitMQMB/src/main/java/com/oldrick/RabbitMQMB/controller/MessageController.java
@@ -1,4 +1,23 @@
 package com.oldrick.RabbitMQMB.controller;
 
+import com.oldrick.RabbitMQMB.config.RabbitMqConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MessageController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestBody String message) {
+        rabbitTemplate.convertAndSend(RabbitMqConfig.QUEUE_NAME, message);
+        return "Message sent to the RabbitMQ queue: " + message;
+    }
 }
